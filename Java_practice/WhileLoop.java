03c7fad17394e9caeb111b8c7bcd2c95f4352d64
ThiyagarajanR <ThiyagarajanR@users.noreply.github.com>
2024-11-16 21:36:14 +0000

    WhileLoop

---
+++ Java_practice/WhileLoop.java
@@ -0,0 +1,15 @@
+public class WhileLoop {
+    public static void main(String[] args)
+    {
+        int x = 1;
+        System.out.println("Before loop");
+
+        while(x < 4)
+        {
+            System.out.println("Value of x is "+x);
+            x++;
+        }
+
+        System.out.println("After the loop");
+    }
+}
